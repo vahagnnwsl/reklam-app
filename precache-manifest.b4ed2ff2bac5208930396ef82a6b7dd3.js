87f83e70185f3d508662f5ebc1c58ae8ec143559
vahagnnwsl <devvahagn@gmail.com>
2019-12-27 16:33:55 +0400

    Updates

---
+++ precache-manifest.b4ed2ff2bac5208930396ef82a6b7dd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d17317ade4d299b0895555eb092d20",
+    "url": "/reklam-app/index.html"
+  },
+  {
+    "revision": "9f68d948255b7d7c450f",
+    "url": "/reklam-app/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "9c6d3d81261892a55736",
+    "url": "/reklam-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "9f68d948255b7d7c450f",
+    "url": "/reklam-app/static/js/2.d3830a02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reklam-app/static/js/2.d3830a02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c6d3d81261892a55736",
+    "url": "/reklam-app/static/js/main.e53a128d.chunk.js"
+  },
+  {
+    "revision": "8695e20392899951edf7",
+    "url": "/reklam-app/static/js/runtime-main.ddebd7c2.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/reklam-app/static/media/persik.4e1ec840.png"
+  }
+]);
